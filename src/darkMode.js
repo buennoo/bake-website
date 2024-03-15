a661aa2cfd4e50fa72cc73b05ba380013e71466a
buennoo <127790828+buennoo@users.noreply.github.com>
2024-03-15 17:52:52 +0100

    update dark mode

--- src/darkMode.js
+++ src/darkMode.js
@@ -1,21 +1,36 @@
-var allElements = document.querySelectorAll('*');
-var ifDark = false;
+let ifDark = localStorage.getItem("ifDark");
+const darkModeButton = document.querySelector("#dark-mode-button");
 
-function darkMode() {
-    if(!ifDark) {
-        for (el of allElements) {
-            el.style.backgroundColor = "#28282B";
-            el.style.color = "white";
-        }
-        ifDark = true;
+const darkMode = () => {
+    let allElements = document.querySelectorAll('*');
+    for (el of allElements) {
+        el.style.backgroundColor = "#28282B";
+        el.style.color = "white";
     }
-    else {
-        for (el of allElements) {
-            el.style.backgroundColor = "";
-            el.style.color = "";
-        }
-        ifDark = false;
-    }
+    localStorage.setItem("ifDark", "true");
 }
 
+const lightMode = () => {
+    let allElements = document.querySelectorAll('*');
+    for (el of allElements) {
+        el.style.backgroundColor = "";
+        el.style.color = "";
+    }
+    localStorage.setItem("ifDark", null);
+}
 
+darkModeButton.addEventListener("click", () => {
+    ifDark = localStorage.getItem("ifDark");
+    if (ifDark !== "true") {
+        darkMode();
+    }
+    else {
+        lightMode();
+    }
+})
+
+/*
+decided not to make an extra class for now and everything
+is set dynamically because dark mode porbably won't look
+the same on every page in the future
+*/
