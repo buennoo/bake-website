7625c61c0abc6d3e0a3f2fadeb76daf2d7061dac
buennoo <127790828+buennoo@users.noreply.github.com>
2024-04-21 10:58:53 +0200

    add dark mode switch function
    still need to add style for dark-mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from "./components/Header";
@@ -10,9 +10,42 @@ import Contact from './pages/Contact';
 import Recipe from './pages/Recipe';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const getDarkMode = () => {
+    // try catch
+    try {
+      if(localStorage.getItem('darkMode') !== null) {
+        const getDark = localStorage.getItem('darkMode');
+        const dark = JSON.parse(getDark);
+        setDarkMode(dark);
+      }
+    } catch (err) {
+      console.error('darkMode not found in localStorage ',err)
+    }
+  }
+
+  const toggleDarkMode = () => {
+    setDarkMode(previous => {
+      const newMode = !previous;
+      try {
+        localStorage.setItem('darkMode', JSON.stringify(newMode));
+        return newMode;
+      } catch (err) {
+        console.error('Not able to set item in local Storage ', err);
+      }
+      return previous;
+    });
+  };
+
+  useEffect(() => {
+    getDarkMode();
+  }, [])
+
+
   return (
       <div className="page-wrap">
-        <Header />
+        <Header switchDarkMode={toggleDarkMode} isDarkMode={darkMode} />
         <Navbar />
         <Main>
             <Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import { MoonStarsFill, SearchHeart } from 'react-bootstrap-icons';
+import { MoonStarsFill, SearchHeart, SunFill } from 'react-bootstrap-icons';
 import '../styles/Header.css'
 import { Link } from 'react-router-dom';
 
-export default function Header() {
+export default function Header({ switchDarkMode, isDarkMode }) {
 
     const handleSearch = (event) => {
         event.preventDefault();
@@ -14,18 +14,20 @@ export default function Header() {
                 <Link to="/">bake coze</Link>
             </div>
             <div class="header-aside">
-                <button id="dark-mode-button">
-                    <MoonStarsFill/>
+                <button id="dark-mode-button" onClick={switchDarkMode}>
+                    {isDarkMode ? <SunFill/> : <MoonStarsFill/>}
                 </button>
                 <div class="login">
                     <button id="loginButton">Login</button>
                 </div>
                 <div class="search-container">
-                    <form class="search-bar" onsubmit={handleSearch}>
+                    <form class="search-bar" onSubmit={handleSearch}>
                         <div class="md-form mt-0">
                             <input class="form-control" id="searchBar" type="text" placeholder="Search.." aria-label="Search"/>
                         </div>
-                        <button type="submit"><SearchHeart/></button>
+                        <button type="submit">
+                            <SearchHeart/>
+                        </button>
                     </form> 
                 </div>
             </div>
